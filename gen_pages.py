e3324228fe4b9ba900247e807f4fa8a99009f41f
Iker Ortiz de Luzuriaga <iker.ortiz@dipc.org>
2023-06-12 15:02:41 +0200

    Adding plugin

--- gen_pages.py
+++ gen_pages.py
@@ -1,4 +1,11 @@
 import mkdocs_gen_files
 
-with mkdocs_gen_files.open("test.txt", "w") as f:
-    print("Hello, world!", file=f)
+for total in range(19, 100, 20):
+    filename = f"sample/{total}-bottles.md"
+
+    with mkdocs_gen_files.open(filename, "w") as f:
+        for i in reversed(range(1, total + 1)):
+            print(f"{i} bottles of beer on the wall, {i} bottles of beer  ", file=f)
+            print(f"Take one down and pass it around, **{i-1}** bottles of beer on the wall\n", file=f)
+
+    mkdocs_gen_files.set_edit_path(filename, "gen_pages.py")
