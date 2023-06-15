ca8eb1dd2a958ace8760646c714b6e51cfe08e18
Iker Ortiz de Luzuriaga <iker.ortiz@dipc.org>
2023-06-15 13:06:29 +0200

    Adding plugin

---
+++ calculator/main.py
@@ -0,0 +1,18 @@
+def add(a, b):
+    """Compute and return sum of two numbers
+    
+    Examples: 
+        >>> from calculator import main
+        >>> main.add(5,4)
+    Args:
+        a (float): First number
+        b (float): Second number
+
+    Returns:
+        Sum of a+b
+    """
+    return float(a+b)
+
+def substract(a, b):
+    """Compute and return rest of two numbers"""
+    return float(a-b)
---
+++ docs/calculator/calculator.md
@@ -0,0 +1,12 @@
+## Calculator
+
+This module contains simple math functions
+
+* Add
+* Sub
+* Mul
+* Div
+
+::: calculator.main
+
+
